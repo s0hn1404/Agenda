76f55aa25f8f5b015161a3dafbb2b27f671890aa
s0hn1404 <sohnjunbae@hotmail.com>
2023-11-28 12:39:18 -0300

    first commit

---
+++ src/homepage.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+const agenda = () =>{
+    const home = document.querySelector('#homepage')
+    const main = document.createElement("div")
+    main.classList.add(main)
+    home.appendChild(main)
+}
+
+export default agenda
